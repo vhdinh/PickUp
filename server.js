b3948159c217a33e13fb92e80d4cd8f28fe334a8
Vu Dinh <isthis.vu@gmail.com>
2018-11-07 16:39:44 -0800

    add express server and change port to 3000

---
+++ server.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors'),
+  express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  apiRouter = require('./routes/user');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/pickUp')));
+app.use('/', express.static(path.join(__dirname, 'dist/pickUp')));
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+module.exports = app;
